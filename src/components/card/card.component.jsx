82881d9e1c882461377e19f36819ccfcd1f0de2b
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-12 12:15:59 +0200

    finished the card compairing logic and implemented a flip animation, for now only on hover

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,4 +1,5 @@
 import './card.styles.css';
+import CardBack from '../../assets/card-back.png';
 
 const Card = ({ card, onClick }) => {
   const { id, pictureId, isShown, isInGame } = card;
@@ -9,15 +10,20 @@ const Card = ({ card, onClick }) => {
   };
 
   return (
-    // <button >
     <div className="card-container" onClick={handleClick}>
-      <img
-        id={pictureId}
-        alt={`card-${id}`}
-        src={`https://robohash.org/${pictureId}?set=set1&size=250x250`}
-      />
+      <div className="card-body">
+        <div className="card-front">
+          <img
+            id={pictureId}
+            alt={`card-${id}`}
+            src={`https://robohash.org/${pictureId}?set=set1&size=250x250`}
+          />
+        </div>
+        <div className="card-back">
+          <img src={CardBack} alt={`card-back-${id}`} />
+        </div>
+      </div>
     </div>
-    // </button>
   );
 };
 
