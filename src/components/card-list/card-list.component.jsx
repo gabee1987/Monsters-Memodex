87b313c75ce3e040fac0b1c068b1fdb750e56448
Gabor Koncz <gabee1987@gmail.com>
2022-08-12 08:57:44 +0200

    implementing the successful card pairing is in progress...

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,6 +5,11 @@ import Card from '../card/card.component.jsx';
 import './card-list.styles.css';
 
 const CardList = () => {
+  //const [selectedCards, setSelectedCards] = useState([]);
+  //let selectedCardIds = [];
+
+  const [firstSelectedCard, setfirstSelectedCard] = useState(null);
+  const [secondSelectedCard, setsecondSelectedCard] = useState(null);
   const [cardDeck, setCardDeck] = useState([]);
   const [filteredCardDeck, setFilteredCardDeck] = useState(cardDeck);
   //const { id, pictureId, isShown, isInGame } = monstersDeck;
@@ -15,17 +20,42 @@ const CardList = () => {
 
   useEffect(() => {
     // TODO here comes the filtering of the deck after a successful pairing
-  }, []);
+    const newFilteredCardDeck = cardDeck.filter((card) => {
+      return card.id !== selectedCardId;
+    });
 
-  console.log('card-list component');
-  console.log(cardDeck);
+    console.log('lefut');
+    setFilteredCardDeck(newFilteredCardDeck);
+  }, [cardDeck, selectedCardId]);
 
-  const handleOnClick = (event, param) => {
+  console.log('selectedCardToSet: ', selectedCardId);
+
+  const handleClick = (event) => {
     // event.preventDefault();
     const cardId = event.target.id;
-    console.log('clicked');
-    console.log(cardId);
+
+    // Store the selected card id if no card is selected yet
+    if (selectedCardId == null) {
+      setselectedCardId(cardId);
+    }
+    // If the same id selected again, store it and then remove the selected cards from the main deck
+    else if (selectedCardId === cardId) {
+      console.log('before filter: ', selectedCardId);
+      // const newFilteredCardDeck = cardDeck.filter((card) => {
+      //   return card.pictureId !== selectedCardId;
+      // });
+
+      let newFilteredCardDeck = [5];
+      console.log('filteredCardDeck: ', newFilteredCardDeck);
+      setFilteredCardDeck(newFilteredCardDeck);
+    }
+    // If a different card was selected, delete the selected id
+    else {
+      setselectedCardId(cardId);
+    }
   };
+
+  const removeSelectedCardsFromDeck = ({ selectedCardIds }) => {};
 
   const createCardDeck = () => {
     // TODO need to create some logic around the initial cards, for example a difficulty system where harder difficulty means more card
@@ -51,14 +81,8 @@ const CardList = () => {
 
   return (
     <div className="card-list">
-      {cardDeck.map((card) => {
-        return (
-          <Card
-            key={card.id}
-            monster={card}
-            onClick={(event) => handleOnClick(event, [])}
-          />
-        );
+      {filteredCardDeck.map((card) => {
+        return <Card key={card.id} monster={card} onClick={handleClick} />;
       })}
     </div>
   );
